5e3aa1eee58a8b8e423e0cb862f7902400561de7
HilarioNengareJr <38634516+HilarioNengareJr@users.noreply.github.com>
2022-11-09 10:34:17 +0200

    GroupBy.md
     List the number of instructors in each department. Sort the list.

--- group_by.sql
+++ group_by.sql
@@ -1,2 +1,5 @@
+
+/*List the number of instructors in each department. Sort the list.*/
+
 SELECT dept_name, COUNT(dept_name) AS number_of_instructors
 FROM instructor GROUP BY dept_name ORDER BY number_of_instructors DESC;
